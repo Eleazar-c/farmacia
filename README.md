4c257908094996decacded0062f8e843d0ffa650
Eleazar <eleazarcuellar5@gmail.com>
2022-03-18 19:27:18 -0600

    Inicio del proyecto
    Este punto tiene creado el fronted del login pero no tiene funcionalidad y se hizo el css el javascript y se descargo los iconos yse agrego el link de google fonts

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-farmacia
+Este proyecto esta realizado por medio de un tutorial del canal de youtube CODER WAR (https://www.youtube.com/channel/UCB4aBNO4RWvrBtkZWkD4jUw) todos los creditos a CODER WAR 
---
+++ vista/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <!-- Este son los iconos y tiene tambien tiene que estar la carpeta webfonts  -->
+    <link rel="stylesheet" href="../css/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <img src="../img/wave.png" alt="" class="wave">
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/bg.svg" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="" method="post">
+                <img src="../img/logo.png" alt="">
+                <div class="input-div dni">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>DNI</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fa-solid fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>contraseña</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                </div>
+                <a href="">created Warpiece</a>
+                <input type="submit" value="iniciar sesion" class="btn">
+            </form>
+        </div>
+    </div>
+    <script src="../js/main.js"></script>
+</body>
+</html>
